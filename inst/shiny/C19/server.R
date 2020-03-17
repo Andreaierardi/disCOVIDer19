81bac1f802b997079e1e715f3d6aed03fa7bc779
Andrea Ierardi <andreierardi@gmail.com>
2020-03-17 21:53:11 +0100

    Edited

--- inst/shiny/C19/server.R
+++ inst/shiny/C19/server.R
@@ -8,11 +8,26 @@ library(httr)
 
 server <- function(input, output, session) {
   
-  regionTS <- covid19:::get_regionTS()
-  countryTS <- covid19:::get_countryTS()
+  get_country <- function() {
+    data <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv",
+                     header = TRUE)
+    
+    data$data <- as.Date(as.character(data$data), format = "%Y-%m-%d")
+    data["data_seriale"] = c(1:length(data$data)-1)
+    return(data)
+    
+  }
+  
+  
+  source("exe_fit.R")
+  source("get_intesivecare_cap.R")
+  source("get_provTS.R")
+  source("get_regionTS.R")
 
-  provTS <- covid19:::get_provTS()
-  intensivecare_capacity = covid19:::get_intensivecare_cap(regionTS)
+  countryTS <- get_country()
+  regionTS <- get_regionTS()
+  provTS <- get_provTS()
+  intensivecare_capacity = get_intensivecare_cap(regionTS)
   
   source(file.path("server", "global/shinyjs.R"),  local = TRUE)$value
   

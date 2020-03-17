5f189722a355356910abc3ff6834291dba31a554
Andrea Ierardi <andreierardi@gmail.com>
2020-03-17 16:56:27 +0100

    up

--- inst/shiny/C19/server/get_countryTS.R
+++ inst/shiny/C19/server/get_countryTS.R
@@ -3,6 +3,8 @@
 #' @return the national dataset
 #'
 #' @export
+
+
 get_countryTS <- function() {
   data <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv",
                    header = TRUE)

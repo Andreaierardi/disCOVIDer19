79a32b747189802f0d7543fa9b1662e24644b6da
Andreaierardi <andreierardi@gmail.com>
2020-10-26 11:55:49 +0100

    update 26-10-2020

--- inst/shiny/C19/ui/tabs/0.tab_home.R
+++ inst/shiny/C19/ui/tabs/0.tab_home.R
@@ -7,98 +7,6 @@ shinydashboard::tabItem(tabName = "home",
                         fluidRow(
                           column(6,
                                  
-                                 shinydashboard::box(title="Number of cases", solidHeader = T,
-                                                     width = NULL,
-                                                     color = "red",
-                                                     tags$head(tags$style(HTML('
- /* tabBox background */
-
- .nav-tabs-custom > .nav-tabs > li.active {
-     border-top-color: red !important;
- }
- 
-  .btn-default {
-    background-color: #dd4b39 !important;
-    color: white !important;
-    border-color: #dd4b39 !important;
-}
-                                               '))),
-                                                     
-                                                     shinydashboard::tabBox(
-                                                       width = 12,
-                                                       # height = "250px",
-                                                       title = fluidRow(
-                                                         div(style="display: inline-block;vertical-align:top;",
-                                                             tags$head(tags$style("
-                       .jhr{
-                       display: inline;
-                       vertical-align: middle;
-                       padding-left: 10px;
-                       }")),
-                                                             
-                                                             selectInput(inputId = "map_value",
-                                                                         label = NULL,
-                                                                         choices = c("absolute", "proportion", "density", "growth"),
-                                                                         width="150px"),
-                                                             
-                                                             #   shinyWidgets::pickerInput(inputId = "map_value",
-                                                             #          label = NULL,
-                                                             #          choices = c("absolute", "proportion", "density"),
-                                                             #          choicesOpt = list(content = c(
-                                                             #            sprintf("<img src='https://www.color-hex.com/palettes/4699.png' width=30px><div class='jhr'>%s</div></img>", factor("absolute",levels=c("absolute","proportion","density")) ),
-                                                             #            sprintf("<img src='https://www.color-hex.com/palettes/12521.png' width=30px><div class='jhr'>%s</div></img>", factor("proportion",levels=c("absolute","proportion","density")) ),
-                                                             #            sprintf("<img src='https://www.color-hex.com/palettes/30573.png' width=30px><div class='jhr'>%s</div></img>", factor("density",levels=c("absolute","proportion","density")) )
-                                                             #          )))
-                                                             
-                                                         ),
-                                                          div(style="display: inline-block;vertical-align:top;",
-                                                             
-                                                             # shinyWidgets::circleButton("show", icon=icon("search-plus"),size="sm")
-                                          
-                                                             # actionButton(inputId="show", icon=icon("search-plus"), label="")
-                                                             
-                                                          )
-                                                       ),
-                                                       # The id lets us use input$tabset1 on the server to find the current tab
-                                                       id = "tabset1",
-                                                       tabPanel("By Province",
-                                                                shinycssloaders::withSpinner(
-                                                                  highcharter::highchartOutput('map_province',
-                                                                                               width = "100%",
-                                                                                               height = "510px"),
-                                                                  color="#dd4b39"
-                                                                )
-                                                       ),
-                                                       tabPanel("By Region",
-                                                                shinycssloaders::withSpinner(
-                                                                  highcharter::highchartOutput('map_region',
-                                                                                               width = "100%",
-                                                                                               height = "510px"
-                                                                  ),
-                                                                  color="#dd4b39"
-                                                                )
-                                                       )
-                                                     ),
-                                                     height=NULL, status="danger"
-                                 )
-                                 
-                          ),
-                          column(6,
-                                 
-                                 
-                                 fluidRow(
-                                   shinydashboard::valueBox(tail(countryTS$Italy$totale_casi,1), "Total cases", icon = icon("notes-medical"),
-                                                            color = "red", width = 6),
-                                   shinydashboard::valueBox(tail(countryTS$Italy$terapia_intensiva,1), "Intensive care", icon = icon("procedures"),
-                                                            color = "orange", width = 6)
-                                 ),
-                                 fluidRow(
-                                   shinydashboard::valueBox(tail(countryTS$Italy$totale_ospedalizzati,1), "Hospitalised", icon = icon("hospital"),
-                                                            color = "yellow", width = 6),
-                                   shinydashboard::valueBox(tail(countryTS$Italy$isolamento_domiciliare,1), "Home isolation", icon = icon("home"),
-                                                            color = "maroon", width = 6)
-                                 ),
-                                 
                                  shinydashboardPlus::flipBox(
                                    id = 1,
                                    width = 12,
@@ -177,6 +85,24 @@ shinydashboard::tabItem(tabName = "home",
                                      
                                      
                                    )
+                                 )
+                                 
+                                 
+                          ),
+                          column(6,
+                                 
+                                 
+                                 fluidRow(
+                                   shinydashboard::valueBox(tail(countryTS$Italy$totale_casi,1), "Total cases", icon = icon("notes-medical"),
+                                                            color = "red", width = 6),
+                                   shinydashboard::valueBox(tail(countryTS$Italy$terapia_intensiva,1), "Intensive care", icon = icon("procedures"),
+                                                            color = "orange", width = 6)
+                                 ),
+                                 fluidRow(
+                                   shinydashboard::valueBox(tail(countryTS$Italy$totale_ospedalizzati,1), "Hospitalised", icon = icon("hospital"),
+                                                            color = "yellow", width = 6),
+                                   shinydashboard::valueBox(tail(countryTS$Italy$isolamento_domiciliare,1), "Home isolation", icon = icon("home"),
+                                                            color = "maroon", width = 6)
                                  )
                                  
                                  
